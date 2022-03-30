8538d86758cddad10ca939149a49ed51d5e7de6a
Joy Shen <ys866@cornell.edu>
2022-03-30 16:42:52 +0000

    hard-code implementation

--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,8 @@
 
   <nav>
     <ul>
-        <li class="<?php echo $nav_plants_data; ?>"><a href="/">Playful Plants Data</a>
-        <li class="<?php echo $nav_new_entry_form; ?>"><a href="/add-new-plants-form">Add-New-Plants Form</a>
+        <li class="<?php echo $nav_plants_data; ?>"><a href="/">Plants Information</a>
+        <li class="<?php echo $nav_new_entry_form; ?>"><a href="/add-new-plants-form">Add New Plants</a>
     </ul>
   </nav>
 </header>
