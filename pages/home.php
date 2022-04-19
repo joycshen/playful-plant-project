8b81b022992f52aefc7e17fc355b13596312bc6b
Joy Shen <ys866@cornell.edu>
2022-04-19 17:14:46 +0000

    filter and sort implementation

--- pages/home.php
+++ pages/home.php
@@ -6,6 +6,99 @@ $nav_new_entry_form = "active_page";
 // open database
 $db = init_sqlite_db('db/site.sqlite', 'db/init.sql');
 
+// if (isset($_GET['submit-sort'])) {
+//   $sort = $_GET['sort'];
+// }
+
+//get tag
+// $tag_id = $_GET['id'] ?? NULL;
+
+// if ($tag_id) {
+//   $tags = exec_sql_query(
+//     $db,
+//     "SELECT * FROM tags;",
+//     array(':id' => $tag_id)
+//   )->fetchAll();
+
+//   $tag = $tags[0];
+// }
+
+//Filters
+$feedback_class = 'hidden';
+
+// values
+// $exploratory_constructive_play_1 = '';
+// $exploratory_sensory_play_1 = '';
+// $physical_play_1 = '';
+// $imaginative_play_1 = '';
+// $restorative_play_1 = '';
+// $play_with_rules_1 = '';
+// $bio_play_1 = '';
+$perennial ='';
+$full_sun = '';
+$partial_shade = '';
+$full_shade = '';
+$hardiness_zone_range = '';
+$sort = '';
+
+// sticky values
+// $sticky_exploratory_constructive_play_1 = '';
+// $sticky_exploratory_sensory_play_1 = '';
+// $sticky_physical_play_1 = '';
+// $sticky_imaginative_play_1 = '';
+// $sticky_restorative_play_1 = '';
+// $sticky_play_with_rules_1 = '';
+// $sticky_bio_play_1 = '';
+$sticky_perennial ='';
+$sticky_full_sun = '';
+$sticky_partial_shade = '';
+$sticky_full_shade = '';
+$sticky_sort = '';
+
+if (isset($_GET['submit-filter'])) {
+
+  // $exploratory_constructive_play_1 = $_GET['exploratory_constructive_play'];
+  // $exploratory_sensory_play_1 = $_GET['exploratory_sensory_play'];
+  // $physical_play_1 = $_GET['physical_play'];
+  // $imaginative_play_1 = $_GET['imaginative_play'];
+  // $restorative_play_1 = $_GET['restorative_play'];
+  // $play_with_rules_1 = $_GET['play_with_rules'];
+  // $bio_play_1 = $_GET['bio_play'];
+  $perennial = $_GET['perennial'];
+  $full_sun = $_GET['full_sun'];
+  $partial_shade = $_GET['partial_shade'];
+  $full_shade = $_GET['full_shade'];
+  $sort = $_GET['sort'];
+
+  // $upload = $_FILES['img-file'];
+
+  $form_valid = True;
+
+  if (empty($sort) && empty($perennial) && empty($full_sun) && empty($partial_shade) && empty($full_shade)) {
+    $form_valid = False;
+    $feedback_class = '';
+    $sticky_sort = (empty($sort) ? '' : 'checked');
+  }
+
+  if ($form_valid) {
+    // form is valid --> sticky values
+    // $sticky_exploratory_constructive_play_1 = (empty($exploratory_constructive_play_1) ? '' : 'checked');
+    // $sticky_exploratory_sensory_play_1 = (empty($exploratory_sensory_play_1) ? '' : 'checked');
+    // $sticky_physical_play_1 = (empty($physical_play_1) ? '' : 'checked');
+    // $sticky_imaginative_play_1 = (empty($imaginative_play_1) ? '' : 'checked');
+    // $sticky_restorative_play_1 = (empty($restorative_play_1) ? '' : 'checked');
+    // $sticky_play_with_rules_1 = (empty($play_with_rules_1) ? '' : 'checked');
+    // $sticky_exploratory_constructive_play_1 = (empty($exploratory_constructive_play_1) ? '' : 'checked');
+    // $sticky_bio_play_1 = (empty($bio_play_1) ? '' : 'checked');
+    $sticky_perennial =  (empty($perennial) ? '' : 'checked');
+    $sticky_full_sun =  (empty($full_sun) ? '' : 'checked');
+    $sticky_partial_shade =  (empty($partial_shade) ? '' : 'checked');
+    $sticky_full_shade =  (empty($full_shade) ? '' : 'checked');
+    $sticky_sort = (empty($sort) ? '' : 'checked');
+    $feedback_class = 'hidden';
+  }
+}
+
 //query pieces
 // $sql_select_part = 'SELECT * FROM entries ';
 $sql_select_part = "SELECT
@@ -31,68 +124,36 @@ entry_tags.tag_id AS 'tags.id'
 FROM
 entry_tags
 LEFT OUTER JOIN entries ON (entry_tags.entry_id = entries.id)
-LEFT OUTER JOIN tags ON (entry_tags.tag_id = tags.id);";
+LEFT OUTER JOIN tags ON (entry_tags.tag_id = tags.id) ";
 $sql_where_part = '';
-$sql_sort_part = ' ORDER BY name_colloquial ASC;';
+$sql_sort_part = ' ORDER BY entries.name_colloquial ASC;';
 $sql_filter_array = array();
 $sql_tag_array = array();
 
-//get tag
-// $tag_id = $_GET['id'] ?? NULL;
-
-// if ($tag_id) {
-//   $tags = exec_sql_query(
-//     $db,
-//     "SELECT * FROM tags;",
-//     array(':id' => $tag_id)
-//   )->fetchAll();
-
-//   $tag = $tags[0];
-// }
-
 //Filters
-$filter_exploratory_constructive_play = (bool)($_GET['exploratory_constructive_play'] ?? NULL);
-$filter_exploratory_sensory_play = (bool)($_GET['exploratory_sensory_play'] ?? NULL);
-$filter_physical_play = (bool)($_GET['physical_play'] ?? NULL);
-$filter_imaginative_play = (bool)($_GET['imaginative_play'] ?? NULL);
-$filter_restorative_play = (bool)($_GET['restorative_play'] ?? NULL);
-$filter_play_with_rules = (bool)($_GET['play_with_rules'] ?? NULL);
-$filter_bio_play = (bool)($_GET['bio_play'] ?? NULL);
+$filter_perennial = (bool)($_GET['perennial'] ?? NULL);
+$filter_full_sun = (bool)($_GET['full_sun'] ?? NULL);
+$filter_partial_shade = (bool)($_GET['partial_shade'] ?? NULL);
+$filter_full_shade = (bool)($_GET['full_shade'] ?? NULL);
 
-if ($filter_exploratory_constructive_play) {
-  array_push($sql_filter_array, "(exploratory_constructive_play = '1')");
+if ($filter_perennial) {
+  array_push($sql_filter_array, "(perennial = '1')");
 }
 
-if ($filter_exploratory_sensory_play) {
-  array_push($sql_filter_array, "(exploratory_sensory_play = '1')");
+if ($filter_full_sun) {
+  array_push($sql_filter_array, "(full_sun = '1')");
 }
 
-if ($filter_physical_play) {
-  array_push($sql_filter_array, "(physical_play = '1')");
+if ($filter_partial_shade) {
+  array_push($sql_filter_array, "(partial_shade = '1')");
 }
 
-if ($filter_imaginative_play) {
-  array_push($sql_filter_array, "(imaginative_play = '1')");
+if ($filter_full_shade) {
+  array_push($sql_filter_array, "(full_shade = '1')");
 }
-
-if ($filter_restorative_play) {
-  array_push($sql_filter_array, "(restorative_play = '1')");
-}
-
-if ($filter_play_with_rules) {
-  array_push($sql_filter_array, "(play_with_rules = '1')");
-}
-
-if ($filter_bio_play) {
-  array_push($sql_filter_array, "(bio_play = '1')");
-}
-
-// if (count($sql_filter_array) > 0) {
-//   $sql_where_part = ' WHERE ' . implode(' AND ', $sql_filter_array);
-// }
 
 //tags
-$tag_name = $_GET['tag'];
+// $tag_name = $_GET['tag'];
 // $tag_grass = $_GET['grass'];
 // $tag_vine = $_GET['vine'];
 // $tag_tree = $_GET['tree'];
@@ -101,9 +162,9 @@ $tag_name = $_GET['tag'];
 // $tag_other = $_GET['other'];
 // $tag_name = $_GET['tags.tag_name'];
 
-if ($tag_name) {
-  array_push($sql_tag_array, "('tag = 'shrub')");
-}
+// if ($tag_name) {
+//   array_push($sql_tag_array, "('tag = 'shrub')");
+// }
 
 // if ($tag_grass) {
 //   array_push($sql_tag_array, "('grass' = 'grass')");
@@ -175,7 +236,7 @@ $records = exec_sql_query($db, $sql_query)->fetchAll();
   <article>
   <div class="body-display">
   <div class="row">
-  <div class="titles">
+  <!-- <div class="titles">
     <p>Filters: </p>
   </div>
 
@@ -213,17 +274,24 @@ $records = exec_sql_query($db, $sql_query)->fetchAll();
   </div>
 
   <div>
-  <form id="request-form" method="get" action="/" novalidate>
+  <form id="request-form" method="get" action="/" name="submit-sort" novalidate>
   <div class="sort">
-  <div class="select" onclick="showCheckboxes()">
-  <select name="name" id="name">'
-    <option value="asc" id="asc">Colloquial Name Ascendant (A-Z)</option>
-    <option value="desc" id="desc">Colloquial Name Descendant (Z-A)</option>
+  <div class="select">
+  <select id="name" name="sort">
+    <option name="sort" id="asc">Colloquial Name Ascendant (A-Z)</option>
+    <option name="sort" id="desc">Colloquial Name Descendant (Z-A)</option>
   </select>
+  <div class="forms label_input" role="group" aria-labelledby="sorting">
+        <div>
+              <input type="submit" id="sorting" name="sort"/>
+              <label for="sorting">Sort by Colloquial Name Alphabetically</label>
+            </div>
+        </div>
   </div>
   </div>
+  <input id="filter-submit" class="button1" type="submit" name="submit-sort" value="Sort"/>
   </form>
-  </div>
+  </div> -->
   </div>
   </div>
 
@@ -246,6 +314,42 @@ $records = exec_sql_query($db, $sql_query)->fetchAll();
     <aside>
     <div class="form">
     <div class="column">
+    <div id="feedback" class="feedback <?php echo $feedback_class; ?>">Please choose at least one sorting/filter.</div>
+      <h2>Sort by...</h2>
+      <form id="request-form" method="get" action="/" novalidate>
+
+        <div class="forms label_input" role="group" aria-labelledby="sorting">
+        <div>
+              <input type="checkbox" id="sorting" name="sort" <?php echo $sticky_sort; ?>/>
+              <label for="sorting">Sort by Colloquial Name Alphabetically (A-Z)</label>
+            </div>
+        </div>
+
+      <h2>Filter by...</h2>
+        <div class="forms label_input" role="group" aria-labelledby="growing needs">
+          <div id="TOPO"><h3>Growing Needs and Characteristic: </h3></div>
+
+          <div>
+            <div>
+              <input type="checkbox" id="perennial" name="perennial" <?php echo $sticky_perennial; ?>/>
+              <label for="perennial">Perennial</label>
+            </div>
+            <div>
+              <input type="checkbox" id="full_sun" name="full_sun" <?php echo $sticky_full_sun; ?>/>
+              <label for="full_sun">Full Sun</label>
+            </div>
+            <div>
+              <input type="checkbox" id="partial_shade" name="partial_shade" <?php echo $sticky_partial_shade; ?>/>
+              <label for="partial_shade">Partial Shade</label>
+            </div>
+            <div>
+              <input type="checkbox" id="full_shade" name="full_shade" <?php echo $sticky_full_shade; ?>/>
+              <label for="full_shade">Full Shade</label>
+            </div>
+          </div>
+        </div>
+        <input id="filter-submit" class="button1" type="submit" name="submit-filter" value="Sort and Filter"/>
+      </form>
       <h2>Choose Tag(s)</h2>
           <h3>General Classification: </h3></div>
           <div class="buttons">
